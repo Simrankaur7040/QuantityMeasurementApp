56dda47a7e9cc745ea1b4ac47e059eaefbeaa2d3
Simrankaur7040 <simrankaur22370@gmail.com>
2026-03-27 23:18:39 +0530

    UC1 work

--- measurement/src/test/java/QuantityMeasurementApp/MeasurementApplicationTests.java
+++ measurement/src/test/java/QuantityMeasurementApp/MeasurementApplicationTests.java
@@ -1,13 +1,45 @@
 package QuantityMeasurementApp;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest
+//@SpringBootTest
 class MeasurementApplicationTests {
 
     @Test
-    void contextLoads() {
+    void testFeetEquality_SameValue() {
+        MeasurementApplication.Feet f1=new MeasurementApplication.Feet(89.6);
+        MeasurementApplication.Feet f2=new MeasurementApplication.Feet(89.6);
+
+        assertEquals(f1,f2);
     }
+    @Test
+    void testFeetInEquality_DifferValue(){
+        MeasurementApplication.Feet f1=new MeasurementApplication.Feet(89.6);
+        MeasurementApplication.Feet f2=new MeasurementApplication.Feet(34.6);
+
+        assertNotEquals(f1,f2);
+    }
+
+    @Test
+    void testFeetNullable_NullValue(){
+        MeasurementApplication.Feet f1=new MeasurementApplication.Feet(55.0);
+        assertFalse(f1.equals(null));
+    }
+
+    @Test
+    void testFeetNullEquality_ClassComparison(){
+        MeasurementApplication.Feet f1=new MeasurementApplication.Feet(89.6);
+        assertFalse(f1.equals("Some String"));
+
+    }
+
+    @Test
+    void testFeetNullEquality_SameReference(){
+        MeasurementApplication.Feet f1=new MeasurementApplication.Feet(89.6);
+        assertTrue(f1.equals(f1));
+
+    }
+
 
 }
